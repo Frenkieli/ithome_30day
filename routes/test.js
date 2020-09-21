cfd5358cfa5915f4a9b18fb34cdd0a1eb5adee8e
Frenkieli <49364993+Frenkieli@users.noreply.github.com>
2020-09-21 21:53:59 +0800

    day07: 呼叫API的各種參數與接收！

--- routes/test.js
+++ routes/test.js
@@ -8,4 +8,30 @@ router.post('/', function(req, res, next) {
   });
 });
 
+router.get('/get', function(req, res, next) {
+  res.json({
+    status: 'OK',
+    value: 'hello get！建立好囉！',
+    query: req.query
+  });
+});
+
+router.get('/get/:id', function(req, res, next) {
+  res.json({
+    status: 'OK',
+    value: 'hello get2！建立好囉！',
+    query: req.query,
+    params: req.params
+  });
+});
+
+router.post('/post/:id', function(req, res, next) {
+  res.json({
+    status: 'OK',
+    value: 'hello post！建立好囉！',
+    query: req.query,
+    params: req.params,
+    body: req.body
+  });
+});
 module.exports = router;
