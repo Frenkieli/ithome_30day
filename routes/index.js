3113e79c24b9d2ccd8291092ca73a706b5e0df51
Frenkieli <49364993+Frenkieli@users.noreply.github.com>
2020-09-29 20:39:47 +0800

    day15: EJS，長的跟HTML87%像的樣版語言

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+let rendeData = { title: '什麼都略懂一點，生活更多彩一些', navBar: [
+  {
+    name: '努力',
+  },
+  {
+    name: '堅持',
+  },
+  {
+    name: '入門',
+  },
+  {
+    name: '放棄',
+  },
+]}
+
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '什麼都略懂一點，生活更多彩一些' });
+  res.render('index', rendeData);
 });
 
 router.get('/test', function(req, res, next) {
-  res.render('test');
+  res.render('test', rendeData);
 });
 
 module.exports = router;
